079f9a115b2f229091ce4258b0ed0ab80070c4b8
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-04-08 19:29:17 +0200

    ci: Try deploy

--- source/index.ts
+++ source/index.ts
@@ -1,19 +1,19 @@
-import "./behavior/Behavior";
-import "./behavior/BehaviorMetadata";
-import "./ui/Column";
-import "./ui/Connection";
-import "./ui/Decoy";
-import "./ui/Input";
-import "./ui/Node";
-import "./ui/NodeEditor";
-import "./ui/NodeNoop";
-import "./ui/NodeRow";
-import "./ui/NodeScript";
-import "./ui/NodeSeed";
-import "./ui/Output";
-import "./ui/Scrollable";
-import "./ui/Toolbar";
-import "./ui/Workarea";
+export * from "./behavior/Behavior";
+export * from "./behavior/BehaviorMetadata";
+export * from "./ui/Column";
+export * from "./ui/Connection";
+export * from "./ui/Decoy";
+export * from "./ui/Input";
+export * from "./ui/Node";
+export * from "./ui/NodeEditor";
+export * from "./ui/NodeNoop";
+export * from "./ui/NodeRow";
+export * from "./ui/NodeScript";
+export * from "./ui/NodeSeed";
+export * from "./ui/Output";
+export * from "./ui/Scrollable";
+export * from "./ui/Toolbar";
+export * from "./ui/Workarea";
 import { Workarea } from "./ui/Workarea";
 
 window.addEventListener("load", () => {
--- source/ui/NodeRow.ts
+++ source/ui/NodeRow.ts
@@ -1,54 +1,43 @@
+import { Behavior } from "../behavior/Behavior";
+import { BehaviorMetadata } from "../behavior/BehaviorMetadata";
 import { mustExist } from "../Maybe";
 import { Node } from "./Node";
 import { Output } from "./Output";
-import { SerializedNode } from "./Workarea";
 
 export class NodeRow extends Node {
   #outputIntElements = new Array<Output>();
 
   constructor() {
     super("row", "Row");
+
+    this.hasBehavior = true;
   }
 
   connectedCallback() {
     super.connectedCallback();
 
-    for (let elementIndex = 0; elementIndex < 5; ++elementIndex) {
-      const element = this.addOutput();
-      element.label = `Int #${elementIndex}`;
-      element.value = elementIndex;
-      this.#outputIntElements.push(element);
-    }
+    this.updateBehavior(
+      Behavior.fromCodeFragment(
+        `
+while(this.outputs.length < 5) { 
+  const output = this.addOutput();
+  output.label = \`Output \${this.outputs.length}\`;
+  output.value = this.outputs.length * 2;
+}
+`,
+        new BehaviorMetadata("Row", [], [])
+      )
+    );
+
+    this.rebuildFromMetadata();
   }
 
-  init(initParameters?: SerializedNode) {
-    super.init(initParameters);
+  rebuildFromMetadata() {
+    const behavior = mustExist(this.behavior);
 
-    this.outputs[0].init(initParameters?.outputs[0]);
-    this.outputs[1].init(initParameters?.outputs[1]);
-    this.outputs[2].init(initParameters?.outputs[2]);
-    this.outputs[3].init(initParameters?.outputs[3]);
-    this.outputs[4].init(initParameters?.outputs[4]);
+    this.name = behavior.metadata.title;
 
-    this.updateUi();
-  }
-
-  serialize(): SerializedNode {
-    return {
-      type: "row",
-      id: mustExist(this.nodeId),
-      name: this.name,
-      x: this.x,
-      y: this.y,
-      inputs: this.inputs.map(input => ({
-        id: mustExist(input.columnId),
-        output: input.output ? mustExist(input.output.source.columnId) : null,
-      })),
-      outputs: this.outputs.map(output => ({
-        id: mustExist(output.columnId),
-        inputs: output.inputs.map(connection => mustExist(connection.target.columnId)),
-      })),
-    };
+    // Ignore rest of metadata. We build the node from script.
   }
 }
 
