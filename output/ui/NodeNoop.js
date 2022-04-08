5d9ec5ba2decf8b83fcc566bba71506f51aa166b
oliversalzburg <oliver.salzburg@protonmail.com>
2022-04-08 20:53:54 +0000

    Deploying to gh-pages from @ oliversalzburg/nodes-js@3a02ca30e0f356175df3d75f3f6753278d150915 🚀

--- output/ui/NodeNoop.js
+++ output/ui/NodeNoop.js
@@ -1,34 +1,20 @@
-import {mustExist} from "../Maybe.js";
+import {Behavior} from "../behavior/Behavior.js";
+import {BehaviorMetadata} from "../behavior/BehaviorMetadata.js";
 import {Node} from "./Node.js";
 export class NodeNoop extends Node {
   constructor() {
     super("noop", "Noop");
+    this.hasBehavior = true;
   }
   connectedCallback() {
     super.connectedCallback();
-    this.addInput();
+    this.updateBehavior(Behavior.fromCodeFragment("", new BehaviorMetadata("Noop", [{identifier: "sink", label: "Sink"}], [])));
+    this.rebuildFromMetadata();
   }
   init(initParameters) {
     super.init(initParameters);
     this.inputs[0].init(initParameters?.inputs[0]);
     this.updateUi();
-  }
-  serialize() {
-    return {
-      type: "noop",
-      id: mustExist(this.nodeId),
-      name: this.name,
-      x: this.x,
-      y: this.y,
-      inputs: this.inputs.map((input) => ({
-        id: mustExist(input.columnId),
-        output: input.output ? mustExist(input.output.source.columnId) : null
-      })),
-      outputs: this.outputs.map((output) => ({
-        id: mustExist(output.columnId),
-        inputs: output.inputs.map((connection) => mustExist(connection.target.columnId))
-      }))
-    };
   }
 }
 customElements.define("dt-node-noop", NodeNoop);
