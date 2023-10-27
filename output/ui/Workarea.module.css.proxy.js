41cc1fdacf623adc6bc7cfcbd6d8370069cbb561
oliversalzburg <oliver.salzburg@protonmail.com>
2023-10-27 16:57:07 +0000

    Deploying to gh-pages from @ oliversalzburg/nodes-js@a785b4caff45526a00727609b70501c546700458 🚀

--- output/ui/Workarea.module.css.proxy.js
+++ output/ui/Workarea.module.css.proxy.js
@@ -1,5 +1,5 @@
-export let code = "._workarea_mvzr4_1 {\n  display: block;\n  position: relative;\n  \n  width: 10000px;\n  height: 10000px;\n\n  background-color: #eee;\n\n  z-index:0;\n}\n";
-let json = {workarea: "_workarea_mvzr4_1"};
+export let code = "._workarea_1b6dm_1 {\n  display: block;\n  position: relative;\n\n  width: 10000px;\n  height: 10000px;\n\n  background-color: #eee;\n\n  z-index: 0;\n}\n";
+let json = {workarea: "_workarea_1b6dm_1"};
 export default json;
 if (typeof document !== "undefined") {
   const styleEl = document.createElement("style");
