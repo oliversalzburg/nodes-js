cdd24eaebf0c3dcb15d8fa7aadf4292ffe4cad30
oliversalzburg <oliver.salzburg@protonmail.com>
2024-06-17 15:25:18 +0000

    Deploying to gh-pages from @ oliversalzburg/nodes-js@e2ebce90765071a4caebae5335476da39d2b6f05 🚀

--- output/Async.js
+++
@@ -1,5 +0,0 @@
-export const asyncEventHandler = (eventHandler) => {
-  return (event) => {
-    eventHandler(event).catch(console.error);
-  };
-};
--- output/ui/Decoy.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Decoy.module.css.proxy.js";
-export class Decoy extends HTMLElement {
-  init(parent, initParameters) {
-    this.classList.add(styles.decoy);
-  }
-}
-customElements.define("dt-decoy", Decoy);
